d4653ed7079ff0756b0c1bd0783563dc19e5c73a
Mauricio P. Braga <61766272+mauriciopbraga@users.noreply.github.com>
2020-11-02 01:43:49 -0300

    bot

--- index.py
+++ index.py
@@ -1,15 +1,18 @@
+#libs
 import pyautogui
 import time
 import keyboard
 import random
 import win32api, win32con
 
+#funcao
 def clique(largura, altura):
     win32api.SetCursorPos((largura, altura))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+#loop
 while keyboard.is_pressed('q') == False:
     if pyautogui.pixel(581, 400)[0] == 0:
         clique(581, 400)
